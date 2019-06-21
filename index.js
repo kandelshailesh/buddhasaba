6add99e0307dfc4d9a0fce929c20b47b94cf99ec
Shailesh <shaileshkandel123@gmail.com>
2019-06-21 13:35:39 +0545

    sfadf

--- index.js
+++ index.js
@@ -2858,22 +2858,22 @@ app.post('/createkaathname', function (req, res) {
 // });
 
 
-// app.use(function (req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 // error handler
-// app.use(function (err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render('pages/error');
-// });
+    // render the error page
+    res.status(err.status || 500);
+    res.render('pages/error');
+});
 
 
 app.listen(3003, () =>
