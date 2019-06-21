7060e958acbe66828b4f21a1c98652e59251c400
Shailesh <shaileshkandel123@gmail.com>
2019-06-21 13:23:45 +0545

    sfadf

--- index.js
+++ index.js
@@ -119,22 +119,22 @@ const saltRounds = 10;
 var nepali = require('get-nepali-number');
 
 
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "buddha",
-//     password: "Sha677@#",
-//     database: 'buddhasaba',
-//     multipleStatements: true
-// });
-
 var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "",
-    port: 3308,
+    user: "buddha",
+    password: "Sha677@#",
     database: 'buddhasaba',
     multipleStatements: true
 });
+
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     port: 3308,
+//     database: 'buddhasaba',
+//     multipleStatements: true
+// });
 
 
 getEmployeeNames = function () {
